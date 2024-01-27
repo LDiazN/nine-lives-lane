bad32a05c330e159192dbe4ba5d78d1f15e325f3
Gabrieloide <gabrielfuentes7312@gmail.com>
2024-01-27 08:55:15 -0400

    world curved

--- Assets/Scripts/Road/LevelManager.cs
+++ Assets/Scripts/Road/LevelManager.cs
@@ -37,6 +37,7 @@ public class LevelManager : MonoBehaviour
         {
             AddLevelBlock();
             yield return new WaitForSeconds(TimeToSpawn);
+
             Points p = currentLevelBlocks[0];
             currentLevelBlocks.RemoveAt(0);
             Destroy(p.gameObject);
