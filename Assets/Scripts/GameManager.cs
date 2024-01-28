5c67b397da2657fa467a267a3d9fd14445d32910
Luis Diaz <ldiazn98@gmail.com>
2024-01-28 13:36:56 -0400

    added holes huring player

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -20,7 +20,7 @@ public class GameManager : MonoBehaviour
     }
     private void Start()
     {
-        Player = GameObject.Find("CarBody");
+        Player = GameObject.Find("Car");
     }
 
 }
--- Assets/Scripts/Lifemanager.cs
+++ Assets/Scripts/Lifemanager.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Lifemanager : MonoBehaviour
 {
-    public float CurrentLifes = 7;
+    public int CurrentLifes = 7;
     public static Lifemanager Instance;
+    private bool _canBeHurt = true;
+    [SerializeField] float TimeBetweenHits = 2; // secondss
+
 
     private void Awake()
     {
@@ -17,5 +20,25 @@ public class Lifemanager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    public void TryHurt(int Damage = 1)
+    {
+        if (!_canBeHurt)
+            return;
+
+        CurrentLifes -= Damage;
+        Debug.Log($"Player Hurt! You have {CurrentLifes}");
+        var carController = GameManager.Instance.Player.GetComponent<CarController>();
+        carController.Boink();
+        StartCoroutine(HurtTimer());
+    }
+
+    IEnumerator HurtTimer()
+    {
+        _canBeHurt = false;
+        yield return new WaitForSeconds(TimeBetweenHits);
+        _canBeHurt = true;
+
     }
 }
